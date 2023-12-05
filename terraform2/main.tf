ff93951d64e5ba419d46c9bb8fd724aa190bc773
Grant Esparza <esparzagrant@gmail.com>
2023-12-05 06:24:39 -0800

    add second config for multiple terraform invocations

---
+++ terraform2/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.12.26"
+}
+
+variable "subject" {
+   type = string
+   default = "tfctl-rc"
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  value = "this is namespace dev2, ${var.subject}!"
+}
