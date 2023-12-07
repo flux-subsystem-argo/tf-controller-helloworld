b00365b73e5742ecabb4350b3d6398fff5f20c66
Grant Esparza <grante@liatrio.com>
2023-12-07 07:53:28 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,27 +11,3 @@ variable "subject" {
 output "hello_world" {
   value = "hey hey ya, ${var.subject}!"
 }
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-with-tf-controller"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
