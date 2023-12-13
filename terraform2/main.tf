ffd481be8b4fe986f47b50944f87cb1e05a3e2a0
Grant Esparza <esparzagrant@gmail.com>
2023-12-13 09:26:05 -0800

    feat: add new output to test new changes

--- terraform2/main.tf
+++ terraform2/main.tf
@@ -8,10 +8,10 @@ variable "subject" {
    description = "Subject to hello and branch planner test"
 }
 
-output "hello_world" {
-  value = "this is namespace dev2, ${var.subject}!"
+output "hello_worldZero" {
+  value = "this is namespace dev, ${var.subject}!"
 }
 
-output "hello_world2" {
-  value = "this is namespace dev2, ${var.subject}!"
+output "hello_worldOne" {
+  value = "this is namespace dev, ${var.subject}!"
 }
